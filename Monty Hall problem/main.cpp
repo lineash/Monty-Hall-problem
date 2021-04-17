235f24727f67dfb1483ddcf4070a6f9df81d9a4d
lineash <olol5454@naver.com>
2021-04-17 21:08:21 +0900

    Initial Commit

---
+++ Monty Hall problem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Monty Hall problem
+//
+//  Created by line.ash on 2021/04/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
